5f28968b1420bd909cbdcf46db58655e31042ae7
Hugo Johansson <hugo.johansson@hyperisland.se>
2020-03-04 13:49:16 +0100

    new things 3 new cards with bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ class Fot extends React.Component {
   render() {
     return (
       <div>
-        <footer class="page-footer font-small blue">
-          <div class="footer-copyright text-center py-3">
+        <footer className="page-footer font-small blue">
+          <div className="footer-copyright text-center py-3">
             © 2020 Copyright:
             <p> QG3</p>
           </div>
--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -7,11 +7,23 @@ class mainPage extends React.Component {
         <h1 className="mb-5">Quiz App</h1>
         <div className="container">
           <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-12 text-center">
-              Sample text
+            <div className="text-center card col-sm-12 col-md-4 col-lg-4">
+              <div className="text-center card-body">
+                <p className="text-center card-text text-center">Quiz name</p>
+                <button className="text-center button">Take Quiz</button>
+              </div>
             </div>
-            <div className="col-sm-12 col-md-12 col-lg-12 text-center">
-              Sample text
+            <div className="text-center card col-sm-12 col-md-4 col-lg-4">
+              <div className="text-center card-body">
+                <p className="text-center card-text text-center">Quiz name</p>
+                <button className="text-center button">Take Quiz</button>
+              </div>
+            </div>
+            <div className="text-center card col-sm-12 col-md-4 col-lg-4">
+              <div className="text-center card-body">
+                <p className="text-center card-text text-center">Quiz name</p>
+                <button className="text-center button">Take Quiz</button>
+              </div>
             </div>
           </div>
         </div>
