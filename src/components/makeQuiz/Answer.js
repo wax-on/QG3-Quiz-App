7c9a97ebf386691c388a853e69a04144fe217770
Malin Henningsson <malinhenningsson@live.se>
2020-03-11 14:27:55 +0100

    Indentation.

--- src/components/makeQuiz/Answer.js
+++ src/components/makeQuiz/Answer.js
@@ -2,9 +2,10 @@ import React from "react";
 
 class Answer extends React.Component {
 render () {
-	return (<label>
-		<input type="radio" name="answers" value={this.props.value} onClick={(e)=>this.props.lagra(e)} />
-		{this.props.text}
+	return (
+		<label>
+			<input type="radio" name="answers" value={this.props.value} onClick={(e)=>this.props.lagra(e)} />
+			{this.props.text}
 		</label>
 	)}
 }
